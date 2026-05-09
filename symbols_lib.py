4cb7acb5090564e5918e282f76755c10c322da72
skyskey-tech <kei.andre.kiyo@gmail.com>
2026-05-09 12:39:15 +0200

    Séparation module symbols

---
+++ symbols_lib.py
@@ -0,0 +1,2 @@
+symbols = {'PATH': '.', 'WALL': '#', 'DRAWING': '%', 'START': 'S', 'FINISH': 'F', 'VISITED': 'o'}
+symbols_inversed = {'#':'WALL', '%':'DRAWING', '.':'PATH', 'S':'START', 'F':'FINISH','o':"VISITED"}
